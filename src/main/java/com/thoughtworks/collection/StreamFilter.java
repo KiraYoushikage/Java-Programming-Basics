bbac80170b08ed8fd1099ee8d6522c306b78317a
KiraYous <404465394@qq.com>
2022-07-20 11:43:11 +0800

    filterEndWithS

--- src/main/java/com/thoughtworks/collection/StreamFilter.java
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -6,11 +6,11 @@ import java.util.stream.Collectors;
 public class StreamFilter {
 
     public List<Integer> filterEven(List<Integer> numbers) {
-        return numbers.stream().filter((o)-> (o&1)==0).collect(Collectors.toList());
+        return numbers.stream().filter(o-> (o&1)==0).collect(Collectors.toList());
     }
 
     public List<String> filterEndWithS(List<String> words) {
-        return null;
+        return words.stream().filter(o-> o.endsWith("s")||o.endsWith("S")).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
